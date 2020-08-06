6f33ac02c1cd775085c663577e38a6c2853280bb
Vinicius <vinicvaz95@gmail.com>
2020-08-06 15:32:53 -0300

    saving form data on json

--- backend/app/index/views.py
+++ backend/app/index/views.py
@@ -3,6 +3,14 @@ from .utils import yaml_to_json
 from pynwb import NWBHDF5IO
 import datetime
 import yaml
+import json
+
+
+def get_index(forms, title):
+    for i, e in enumerate(forms):
+        if title in e.keys():
+            return i
+    return -1
 
 
 def index():
@@ -16,7 +24,22 @@ def index():
     schema = yaml_to_json(metadata)
 
     if request.method == 'POST':
-        print(request.json)
+        title = request.json['formTitle']
+        form_index = get_index(schema, title)
+
+        form_schema = schema[form_index]
+        schema_aux = form_schema[title]
+
+        for k, v in request.json['formData'].items():
+            schema_aux['properties'][k]['default'] = v
+
+        schema_json = {
+            title: schema_aux
+        }
+
+        with open('{}_form.json'.format(title), 'w+') as out:
+            json.dump(schema_json, out, indent=4)
+
         return jsonify({'response': 'ok'}), 200
 
     return jsonify({'data':schema})
