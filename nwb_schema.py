8afd452785baad9f72c565ab4a495af641b57eb6
luiztauffer <luiz_tauffer@hotmail.com>
2020-08-13 16:13:16 +0700

    update get_schema function

--- nwb_schema.py
+++ nwb_schema.py
@@ -29,14 +29,35 @@ def get_schema_from_docval(docval):
 
     schema = deepcopy(base_schema)
     for docval_arg in docval['args']:
-        if docval_arg['type'] is str or (isinstance(docval_arg['type'], tuple) and str in docval_arg['type']):
+        # If simple type, store in tuple for next steps comparisons
+        if not isinstance(docval_arg['type'], tuple):
+            item_types = (docval_arg['type'], )
+        else:
+            item_types = docval_arg['type']
+
+        # If item is a float
+        if 'float' in item_types:
+            schema_arg = dict(name=docval_arg['name'], type='number', description=docval_arg['doc'])
+        # If item it is a string
+        elif str in item_types:
             schema_arg = dict(name=docval_arg['name'], type='string', description=docval_arg['doc'])
-            if 'default' in docval_arg:
-                if docval_arg['default'] is not None:
-                    schema_arg.update(default=docval_arg['default'])
-            else:
-                schema['required'].append(docval_arg['name'])
-            schema['properties'].append(schema_arg)
+        # TODO -
+        elif list in item_types:
+            print('skipped ', docval_arg['name'])
+            continue
+        # If item is a pynwb object - recurrently pass child to `get_schema_from_docval()`
+        elif any([getattr(it, '__module__', None).split('.')[0] == pynwb.__name__ for it in item_types]):
+            schema_arg = dict(name=docval_arg['name'], type='object', description=docval_arg['doc'])
+            index = np.where([getattr(it, '__module__', None).split('.')[0] == pynwb.__name__ for it in item_types])[0][0]
+            child_schema = get_schema_from_hdmf_class(hdmf_class=item_types[index])
+            schema_arg.update(properties=child_schema)
+
+        if 'default' in docval_arg:
+            if docval_arg['default'] is not None:
+                schema_arg.update(default=docval_arg['default'])
+        else:
+            schema['required'].append(docval_arg['name'])
+        schema['properties'].append(schema_arg)
 
     if 'allow_extra' in docval:
         schema['additionalProperties'] = docval['allow_extra']
