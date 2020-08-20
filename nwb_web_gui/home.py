b38c15519681985cbb73f8e00b14ff6d34dcca35
Vinicius <vinicvaz95@gmail.com>
2020-08-20 17:22:10 -0300

    add basic file explorer example and component

--- nwb_web_gui/home.py
+++ nwb_web_gui/home.py
@@ -1,6 +1,9 @@
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+from file_explorer import FileExplorer
+from dash.dependencies import Input, Output, State
+from datetime import datetime
 
 
 class Home(html.Div):
@@ -76,10 +79,45 @@ class Home(html.Div):
             ], fluid=True
         )
 
+        """ File Explorer Example """
+        file_schema = [{'key': 'nwb_files/example_file.nwb', 'modified': datetime.utcnow(), 'size': 1.5 * 1024 * 1024}]
+        explorer = dbc.Container(dbc.Row(dbc.Col(FileExplorer(id='explorer', value=file_schema), lg=8), style={'justify-content': 'center'}), fluid=True)
+
+        modal = dbc.Container(
+                    dbc.Row(
+                        [
+                            dbc.Button("Open File Explorer", id="open"),
+                            dbc.Modal(
+                                [
+                                    dbc.ModalHeader("Header"),
+                                    dbc.ModalBody(explorer),
+                                    dbc.ModalFooter(
+                                        dbc.Button("Close", id="close", className="ml-auto")
+                                    ),
+                                ],
+                                id="modal",
+                                size="xl"
+                            ),
+                        ], style={'justify-content': 'center'}
+                    )
+                )
+
         self.children = [
             html.Br(),
             html.H1('NWB Web GUI', style={'text-align': 'center'}),
             html.Br(),
             html.Hr(),
-            row_of_cards
+            row_of_cards,
+            html.Br(),
+            modal
         ]
+
+        @self.parent_app.callback(
+            Output("modal", "is_open"),
+            [Input("open", "n_clicks"), Input("close", "n_clicks")],
+            [State("modal", "is_open")],
+        )
+        def toggle_modal(n1, n2, is_open):
+            if n1 or n2:
+                return not is_open
+            return is_open
