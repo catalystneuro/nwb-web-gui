a0660b44dab33c752f2ae4de81000616628fb342
luiztauffer <luiz_tauffer@hotmail.com>
2020-09-15 13:19:34 +0700

    config from classes

---
+++ config.py
@@ -0,0 +1,25 @@
+
+class Config:
+    """Base config."""
+    SECRET_KEY = 'SECRET_KEY'
+    SESSION_COOKIE_NAME = 'SESSION_COOKIE_NAME'
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    SEND_FILE_MAX_AGE_DEFAULT = 0
+
+
+class ConfigProduction(Config):
+    FLASK_ENV = 'production'
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'DATABASE_URI_PRODUCTION'
+
+
+class ConfigDev(Config):
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = True
+    DATABASE_URI = 'DEV_DATABASE_URI'
+
+    # EXPLORER_PATH = '/home/vinicius/Área de Trabalho/Trabalhos/nwb-web-gui/files'
+    EXPLORER_PATH = r'C:\Users\Luiz\Desktop\data_app'
--- nwb_web_gui/config.py
+++
@@ -1,2 +0,0 @@
-# EXPLORER_PATH = '/home/vinicius/Área de Trabalho/Trabalhos/nwb-web-gui/files'
-EXPLORER_PATH = r'C:\Users\Luiz\Desktop\data_app'
