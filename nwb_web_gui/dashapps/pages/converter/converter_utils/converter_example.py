46de30fc06c4f09632e6a5ab3cf17f96a9c5db32
luiztauffer <luiz_tauffer@hotmail.com>
2020-10-06 15:52:20 +0200

    update example converter

--- nwb_web_gui/dashapps/pages/converter/converter_utils/converter_example.py
+++ nwb_web_gui/dashapps/pages/converter/converter_utils/converter_example.py
@@ -17,14 +17,14 @@ class ExampleNWBConverter(NWBConverter):
             input_schema = json.load(inp)
         return input_schema
 
-    def get_metadata_schema(self, source_paths, conversion_options):
+    def get_metadata_schema(self):
         examples_path = Path(__file__).parent.parent.absolute() / 'example_schemas'
         metadata_schema_path = examples_path / 'schema_metadata_all.json'
         with open(metadata_schema_path, 'r') as inp:
             metadata_schema = json.load(inp)
         return metadata_schema
 
-    def get_metadata(self, source_paths, conversion_options):
+    def get_metadata(self):
         examples_path = Path(__file__).parent.parent.absolute() / 'example_schemas'
         metadata_data_path = examples_path / 'metadata_example_0.json'
         with open(metadata_data_path, 'r') as inp:
