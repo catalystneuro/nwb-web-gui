7f79c52d097618d6eba824ca04965c016876b2e9
Vinicius <vinicvaz95@gmail.com>
2020-12-17 18:19:38 -0300

    viewer container size and basic tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,4 +1,11 @@
 
 
-def test_dummy():
-    assert 1 == 1
+def test_app_is_created(app):
+    assert app.name == 'nwb_web_gui'
+
+def test_converter_200(client):
+    assert client.get('/converter/').status_code == 200
+
+def test_viewer_200(client):
+    assert client.get('/viewer/').status_code == 200
+
