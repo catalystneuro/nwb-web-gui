0b8b0a09f13cc6831a14026952ab1a9f2cee53f4
Vinicius <vinicvaz95@gmail.com>
2020-12-21 17:03:14 -0300

    add shutdown test and cli run

--- nwb_web_gui/routes.py
+++ nwb_web_gui/routes.py
@@ -7,13 +7,16 @@ def home():
     """Landing page."""
     return render_template('home.html')
 
+
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+    if func is not None:
+        #raise RuntimeError('Not running with the Werkzeug Server')
+        func()
+    return
 
-@app.route('/shutdown', methods=['POST', 'GET'])
+
+@app.route('/shutdown/', methods=['POST', 'GET'])
 def shutdown():
     shutdown_server()
     return 'Server down...'
